a94ff82c558d7a076e5999824947f26b4d212e45
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-26 18:22:28 +0100

    Implemented finding country by iso2,iso3,code, and region

--- app/Http/Controllers/Api/V1/CountryController.php
+++ app/Http/Controllers/Api/V1/CountryController.php
@@ -14,13 +14,40 @@ class CountryController extends Controller
 
         $country_count = Country::all()->count();
 
-        //Return all countries if per_page is not provided
+        //Optional route parameters
         $per_page = $request->get('per_page') ?: $country_count;
-
         $continent = $request->get('continent');
+        $iso2 = $request->get('iso2');
+        $iso3 = $request->get('iso3');
+        $code = $request->get('code');
+        $region = $request->get('region');
 
         if(isset($continent)){
+            //Find countries by continent
             return new CountryCollection(Country::whereContinent($continent)->paginate($per_page));
+        }
+
+        if(isset($code)){
+            //Find country by country code
+            //No need to paginate as we expect just one country
+            return new CountryResource(Country::whereCode($code)->first());
+        }
+
+        if(isset($iso2)){
+            //Find country by iso2
+            //No need to paginate as we expect just one country
+            return new CountryResource(Country::whereIso2($iso2)->first());
+        }
+
+        if(isset($iso3)){
+            //Find country by iso3
+            //No need to paginate as we expect just one country
+            return new CountryResource(Country::whereIso3($iso3)->first());
+        }
+
+        if(isset($region)){
+            //Find countries by region
+            return new CountryResource(Country::whereRegion($region)->paginate($per_page));
         }
 
         return new CountryCollection(Country::paginate($per_page));
