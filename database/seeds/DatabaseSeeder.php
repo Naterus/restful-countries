da4a1428f8f050346ad2c9084f4d5881c8846b58
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-27 16:23:14 +0100

    Added role and permission factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Country::class,5)->create();
+        factory(App\Country::class,4)->create();
         factory(App\State::class,50)->create();
+        factory(App\User::class,1)->create();
+        factory(App\Role::class,1)->create();
     }
 }
