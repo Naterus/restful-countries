ca4000afae872ab18e12c2c9fa1bc829c8052f80
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-27 07:00:31 +0100

    Added str_replace to districts path parameter

--- app/Http/Controllers/Api/V1/DistrictController.php
+++ app/Http/Controllers/Api/V1/DistrictController.php
@@ -13,14 +13,14 @@ use Illuminate\Http\Request;
 class DistrictController extends Controller
 {
     public function getDistricts($country,$state,Request $request){
-        $districts = District::whereCountryId($country)->whereStateId($state)->get();
+        $districts = District::whereCountryId(str_replace("-"," ",$country))->whereStateId(str_replace("-"," ",$state))->get();
         return new DistrictCollection($districts);
     }
 
     public function getDistrict($country,$state,$district,Request $request){
-        $country_id = Country::whereName($country)->first();
-        $state_id = State::whereName($state)->whereCountryId($country_id->id ?: null)->first();
-        $district = District::whereCountryId($country_id->id ?: null)->whereStateId($state_id->id ?: null)->whereName($district)->first();
+        $country_id = Country::whereName(str_replace("-"," ",$country))->first();
+        $state_id = State::whereName(str_replace("-"," ",$state))->whereCountryId($country_id->id ?: null)->first();
+        $district = District::whereCountryId($country_id->id ?: null)->whereStateId($state_id->id ?: null)->whereName(str_replace("-"," ",$district))->first();
         return new DistrictResource($district);
     }
 }
