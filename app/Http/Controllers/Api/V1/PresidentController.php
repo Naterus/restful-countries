121a52d5121fe1f6b885f591d90470f312617fb9
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-28 07:41:21 +0100

    Added President api controller

---
+++ app/Http/Controllers/Api/V1/PresidentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PresidentController extends Controller
+{
+    public function getPresidents(){
+
+    }
+}
