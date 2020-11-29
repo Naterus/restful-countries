74108093623d4e53cb93f4aa2347717b03224b6a
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-29 13:53:23 +0100

    Removed css and image storage link

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,8 +80,6 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('images') => storage_path('app/public/images'),
-        public_path('css') => storage_path('app/public/css'),
     ],
 
 ];
