67ac899dcd83397a045812a572580ac568cf2898
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-11-30 07:32:02 +0100

    Added country and president relationship

--- app/President.php
+++ app/President.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class President extends Model
 {
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
     protected $guarded = ["id"];
 }
