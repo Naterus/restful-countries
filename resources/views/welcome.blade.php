bc444b4546d5fe66e1ba2995bcfdddb8629651da
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-12-02 08:41:02 +0100

    Removed home page form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,83 +16,16 @@
                             <div class="title-heading text-center text-white">
                                 <h1 class="heading font-weight-bold mb-4">All 	Countries Repository</h1>
                                 <h6 class="small-title text-uppercase text-light mb-3">Get countries information like states, covid19 stats, presidents, flag, population and others via Restful Api.</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="home-form-position">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="home-registration-form p-4 mb-3">
-                                    <form class="registration-form">
-                                        <div class="row">
-
-                                            <div class="col-lg-6 col-md-6">
-                                                <div class="registration-form-box">
-                                                    <select id="country-select" class="form-control" onchange="initStates()">
-                                                        <option selected disabled>Countries</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6 col-md-6">
-                                                <div class="registration-form-box">
-                                                    <select  id="state-select" name="" class="form-control">
-]                                                       <option selected disabled>States</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-
-                                </div>
                                 <div class="text-center" style="margin-top: 50px;">
                                     <a href="{!! route("documentation",env("APP_VERSION")) !!}" class=""><button class="btn btn-primary">Explore Endpoints</button></a>
                                 </div>
                             </div>
                         </div>
-
                     </div>
-
                 </div>
             </div>
         </div>
     </section>
     <!-- end home -->
 @endsection
-@section("page-script")
-    <script>
-        //Populate countries select on page load
-        $(document).ready(function(){
-            $.get('http://localhost:8081/restful_countries/public/api/v1/countries',function(countries){
 
-                $.each(countries.data,function(key,value){
-                    $('#country-select')
-                        .append($("<option></option>")
-                            .attr("value", value.name)
-                            .text(value.name));
-                });
-
-                //Select first country returned
-                $("#country-select").val(countries.data[0].name);
-                //Populate currently selected country states
-                initStates();
-            });
-        });
-
-        function initStates(){
-            let country=$("#country-select").val();
-
-            //Remove previous loaded options
-            $('#state-select option:gt(0)').remove();
-            $('#district-select option:gt(0)').remove();
-            $.get('http://localhost:8081/restful_countries/public/api/v1/countries/'+country+'/states',function(states){
-
-                $.each(states.data,function(key,value){
-                    $('#state-select')
-                        .append($("<option></option>")
-                            .attr("value", value.name)
-                            .text(value.name));
-                });
-            });
-        }
-    </script>
-@endsection
