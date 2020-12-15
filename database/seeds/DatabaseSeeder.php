b3d658371ec562fb177f83ca2e32458e692ed651
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2020-12-15 21:02:01 +0100

    Fixed database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Country;
+use App\Covid19;
+use App\Helpers\Helper;
 use App\Permission;
-use App\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,7 +20,20 @@ class DatabaseSeeder extends Seeder
         factory(App\State::class,50)->create();
         factory(App\Role::class,1)->create();
         factory(App\User::class,1)->create();
-        factory(App\Permission::class,17)->create();
+        foreach (helper::instance()->appOperations() as $operation){
+            Permission::create([
+                "role_id" => 1,
+                "permission" => $operation
+            ]);
+        }
+
+        $countries = Country::all();
+        foreach ($countries as $country){
+            Covid19::create([
+                "country_id" => $country->id,
+                "updated_by" => 1
+            ]);
+        }
 
     }
 }
