42df3fb92a0cd2135a96a859b6f97d7960c8ee68
Nathan Dauda Aluwong <nathandauda27@gmail.com>
2021-02-04 20:00:41 +0100

    Loaded vue components

--- public/js/app.js
+++ public/js/app.js
@@ -20794,8 +20794,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\restful-countries-1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\restful-countries-1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/nate/lampstack-7.4.8-0/apache2/htdocs/restful-countries/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/nate/lampstack-7.4.8-0/apache2/htdocs/restful-countries/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
